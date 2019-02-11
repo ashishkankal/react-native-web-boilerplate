fbc1030ba8fd910f2dfacf64df60d1c155663eb3
Ashish Kankal <aurelioashish@gmail.com>
2019-02-12 00:47:23 +0530

    Added store to react-native

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import Root from "./app/native/containers/Root";
+import configureStore from "./app/store/configureStore.prod.js";
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from "./app.json";
+const store = configureStore();
+
+class AppIndex extends Component {
+  render() {
+    return <Root store={store} />;
+  }
+}
+
+AppRegistry.registerComponent(appName, () => AppIndex);
